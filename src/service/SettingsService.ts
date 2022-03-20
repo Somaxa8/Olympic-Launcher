1a88cc2bfe2a1ae7f631eadbefa94d9b1891555e
somaxa8 <silviofrancoxa8@gmail.com>
2022-03-20 13:30:45 -0500

    Implemented FSR in client configuration

--- src/service/SettingsService.ts
+++ src/service/SettingsService.ts
@@ -5,6 +5,7 @@ import WineService from "@/service/WineService";
 import Store from "electron-store";
 import log from "loglevel";
 import NotificationService from "@/service/NotificationService";
+import SystemTool from "@/service/tools/SystemTool";
 
 export default class SettingsService {
 
@@ -15,10 +16,15 @@ export default class SettingsService {
 
 
         if (settings.dxvk && !DxvkService.isEnable()) await DxvkService.enable()
-        if (settings.vkd3d && !Vkd3dService.isEnable()) await Vkd3dService.enable()
-
         if (!settings.dxvk && DxvkService.isEnable()) await DxvkService.disable()
+
+        if (settings.vkd3d && !Vkd3dService.isEnable()) await Vkd3dService.enable()
         if (!settings.vkd3d && Vkd3dService.isEnable()) await Vkd3dService.disable()
+
+        if (settings.fsr && !SettingsService.isFSREnable()) await SettingsService.enableFSR(settings)
+        if (!settings.fsr && SettingsService.isFSREnable()) await SettingsService.disableFSR(settings)
+
+        await SettingsService.setFSRStrength(settings)
 
         settings.wineVersions = await WineService.getLocalBuildsWine()
         const currentWine = new Store().get("wine-version") as string
@@ -37,6 +43,27 @@ export default class SettingsService {
 
     static getSettings(): Settings {
         return new Store().get("settings") as Settings
+    }
+
+    static async enableFSR(settings: Settings) {
+        process.env.WINE_FULLSCREEN_FSR = "1"
+        settings.fsr = true
+        log.info("FSR enabled")
+    }
+
+    static async disableFSR(settings: Settings) {
+        process.env.WINE_FULLSCREEN_FSR = "0"
+        settings.fsr = false
+        log.info("FSR disabled")
+    }
+
+    static async setFSRStrength(settings: Settings) {
+        process.env.WINE_FULLSCREEN_FSR_STRENGTH = settings.fsrSharpness.toString()
+        log.info("set FSR strength to: ", settings.fsrSharpness)
+    }
+
+    static isFSREnable(): boolean {
+        return process.env.WINE_FULLSCREEN_FSR != undefined && process.env.WINE_FULLSCREEN_FSR == "1"
     }
 
 
--- src/service/tools/SystemTool.ts
+++ src/service/tools/SystemTool.ts
@@ -2,6 +2,7 @@ import {env, execPath} from "process";
 import {homedir} from "os";
 import { exec } from "child_process";
 import { promisify } from "util";
+import log from "loglevel";
 
 export default class SystemTool {
 
@@ -9,8 +10,9 @@ export default class SystemTool {
     static readonly flatpakHome = env.XDG_DATA_HOME?.replace("/data", "") || homedir()
     static readonly home = this.isFlatpak ? this.flatpakHome : homedir()
     static readonly MAX_BUFFER = 25 * 1024 * 1024
-    static readonly execOptions = {maxBuffer: SystemTool.MAX_BUFFER, shell: SystemTool.getShell()}
+    static readonly execOptions = {maxBuffer: SystemTool.MAX_BUFFER, shell: SystemTool.getShell(), env: process.env}
     static execAsync = promisify(exec)
+
     static getShell(): string {
         switch (process.platform) {
             case "win32":
